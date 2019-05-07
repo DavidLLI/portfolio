c7d7a69dea8d9bf843e91a00cf2c6a0afc3cba74
David Li <a474247132@gmail.com>
2019-05-07 00:24:27 -0400

    sync

--- src/PortfolioContent/Group4Me.js
+++ src/PortfolioContent/Group4Me.js
@@ -3,13 +3,16 @@ import React, { Component } from 'react';
 import './Group4Me.css';
 
 import group4MeWide from '../Assets/group4MeWide.png';
+import processImg from '../Assets/process.png';
 
 class Group4Me extends Component {
   render() {
     return (
       <div className='group4me-portfolio'>
         {/* Image */}
-        <img className='group4me-thumb-img' src={group4MeWide} />
+        <div className='top-block-group4me'>
+          <img className='group4me-thumb-img' src={group4MeWide} />
+        </div>
 
         {/* Title */}
         <div className='group4me-title group4me-text'>
@@ -18,8 +21,8 @@ class Group4Me extends Component {
 
         {/* Description */}
         <div className='group4me-desc group4me-text'>
-          Group4Me aims to connect international students and local students socially
-          with the consideration of cultural & language barrier.
+          Group4Me aims to connect international students and local students socially with the consideration of <br/>
+          cultural & language barrier.
         </div>
 
         {/* Project Overview */}
@@ -59,6 +62,35 @@ class Group4Me extends Component {
         {/* Project Process */}
         <div className='group4me-subtitle group4me-text'>
             Process
+            <img className='group4me-process' src={processImg} />
+        </div>
+
+        {/* Research */}
+        <div className='group4me-research group4me-text'>
+          <div className='group4me-subtitle'>
+            Research
+          </div>
+          <div className='project-title'>
+            The problem
+          </div>
+          <div className='project-detail'>
+            It is typical to see international students staying in their own cultural groups, 
+            and it has made them struggle living abroad. We then researched on this problem
+            area to seek potential solutions.
+          </div>
+          <div className='project-title'>
+            User Interview
+          </div>
+          <div className='project-detail'>
+            We interviewed 9 international students from 4 different countries.
+          </div>
+          <div className='project-title'>
+            Survey
+          </div>
+          <div className='project-detail'>
+            Our survey reached to 28 international students from 9 different countries. .
+          </div>
+
         </div>
       </div>
     );
